1a85444a687247ec9c3f2a4c0cba64690994875e
soulayman <soulaymanjaa09@gmail.com>
2025-11-15 17:56:00 +0100

    routes protection provider

---
+++ src/components/context/OnlyAuth.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "@/components/context/AuthContext";
+
+const OnlyAuth: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const auth = useContext(AuthContext);
+
+  if (!auth?.token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default OnlyAuth;
